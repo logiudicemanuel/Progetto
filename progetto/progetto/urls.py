f10b173d5988319505b0a7dd0abd34db7099f1bd
Logiudice Manuel <51341719+logiudicemanuel@users.noreply.github.com>
2023-01-17 22:55:37 +0100

    Delete progetto directory

--- progetto/progetto/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from backend import views
-
-urlpatterns = [
-    path('', include('backend.urls')),
-    path('admin/', admin.site.urls),
-]
