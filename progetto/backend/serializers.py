296b1cb93efcd7c698ad21ed3b5b399718bc305a
Logiudice Manuel <51341719+logiudicemanuel@users.noreply.github.com>
2023-01-18 20:40:59 +0100

    Delete progetto directory

--- progetto/backend/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Viaggi
-
-class ViaggiSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Viaggi
-        fields = "__all__"
