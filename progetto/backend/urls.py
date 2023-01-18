b77c879117664bea0fbea24693b617bf30e0b47a
Logiudice Manuel <51341719+logiudicemanuel@users.noreply.github.com>
2023-01-18 20:41:55 +0100

    Add files via upload

---
+++ progetto/backend/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register("api/viaggi-fatti", views.ViaggiFattiViewSet, basename="viaggi-fatti")
+router.register("api/viaggi-da-fare", views.ViaggiDaFareViewSet, basename="viaggi-da-fare")
+urlpatterns = router.urls
