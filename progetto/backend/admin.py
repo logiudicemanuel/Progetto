17c5e3dea2c19870571691808ad52330c96a18b1
Logiudice Manuel <51341719+logiudicemanuel@users.noreply.github.com>
2023-01-24 09:19:01 +0100

    Delete progetto directory

--- progetto/backend/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ViaggiFatti
-from .models import ViaggiDaFare
-# Register your models here.
-admin.site.register(ViaggiFatti)
-admin.site.register(ViaggiDaFare)
